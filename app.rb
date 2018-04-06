9586797609f24c90dc1aedaa568cc464a7fb9d7e
Kelsey Fecho <kfecho30@gmail.com>
2018-04-06 22:19:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! I BUILT THIS"
+    "Welcome to your app!!!! I BUILT THIS! More stuff here for demo!"
   end
 
 end
